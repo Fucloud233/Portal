78d9542dc0110229368d0e2279ce603fd7cb15ea
Fucloud <270928474@qq.com>
2023-01-06 03:52:35 +0800

    修改 主函数 以方便测试

--- Portal/main.cpp
+++ Portal/main.cpp
@@ -1,18 +1,33 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
 
+#include "BlockInfoOperator.h"
 #include "Block.h"
+
+void blockTest() {
+    Block block("Empty", Block::FLOOR);
+    block.saveInfo();
+
+    system("pause");
+}
+
+void OperatorTest() {
+    BlockInfoOperator op;
+
+    system("pause");
+}
 
 int main(int argc, char *argv[])
 {
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
+    //
     //return a.exec();
 
-    Block block("Empty", Block::FLOOR);
-    block.saveInfo();
-    
-    system("pause");
+    OperatorTest();
+
     return 0;
 }
