7b3a57cfe8041efb56a1c22b9290565596cf42ed
Fucloud <270928474@qq.com>
2023-01-14 14:31:00 +0800

    新建 测试函数

---
+++ Portal/TestWindow.cpp
@@ -0,0 +1,15 @@
+#include "TestWindow.h"
+
+#include "MapEditorGraphicsView.h"
+
+TestWindow::TestWindow(QWidget* parent) :
+	QMainWindow(parent) {
+
+	resize(1280, 720);
+
+	MapEditorGraphicsView* mapView = new MapEditorGraphicsView(this);
+	setCentralWidget(mapView);
+	mapView->intialMap();
+
+
+}
---
+++ Portal/TestWindow.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <QMainWindow>
+
+class TestWindow: public QMainWindow {
+public:
+	TestWindow(QWidget* parent = nullptr);
+};
+
